efb90e1d67c2cb96205d847362896449bbdd7be0
Adriano Paganini <adriano.paganini@student.uibk.ac.at>
2026-08-27 18:27:52 +0200

    chore: - tried to fix the errors from Serializable, but failed.

--- android/app/src/main/java/com/paganini/voxvault/dataClass/Recording.kt
+++ android/app/src/main/java/com/paganini/voxvault/dataClass/Recording.kt
@@ -3,22 +3,24 @@ package com.paganini.voxvault.dataClass
 import android.content.Context
 import android.view.View
 import android.widget.TextView
-import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.Serializable
 
-@InternalSerializationApi @Serializable
+@Serializable
 data class Recording(
     var name: String = "",
     var duration: Double = 0.0,
     val latitude: Double = 0.0,
     val longitude: Double = 0.0,
     val timestamp: Long = 0L,
-){
+) {
     fun getView(context: Context): View {
-        val textView =  TextView(context)
-        textView.text = String.format(java.util.Locale.US, "%.1fs - %s", duration, name)
+        val textView = TextView(context)
+        textView.text = String.format(
+            java.util.Locale.US,
+            "%.1fs - %s",
+            duration,
+            name
+        )
         return textView
     }
 }
-
-
